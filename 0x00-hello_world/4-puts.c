a48cb0b4a4e1712c0a2c43c82f99f14e16ab6e17
Stevekab <stephenmuchiri8584@gmail.com>
2023-09-07 09:04:48 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - contains the main function
+ *
+ * Return: its always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
