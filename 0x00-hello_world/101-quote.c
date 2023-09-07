b44366ddae890a1915a89558d8d99cd1011d29ae
Stevekab <stephenmuchiri8584@gmail.com>
2023-09-07 15:04:38 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-#include <stdio.h>
-/** 
- * main - main function
+/**
+ * main - the main function
  *
- * Return: Always 1
+ * Return: alwaysreturns 1 after the execution
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
