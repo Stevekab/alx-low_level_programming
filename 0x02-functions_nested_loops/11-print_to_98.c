0c349ea2b284b31ec375f5c664a1c8af57135d7e
Stevekab <stephenmuchiri8584@gmail.com>
2023-09-13 07:48:03 -0700

    ammendment

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
-			if (i == 98)
-				printf("%d", i);
+			if (i == n)
+				printf("%d",i);
+			printf(", %d", i);
 		}
 	}
 	else if (n == 98)
