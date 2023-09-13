51443f656506a1763c5bfdf2a12c09f2a31a955c
Stevekab <stephenmuchiri8584@gmail.com>
2023-09-13 07:07:23 -0700

    summing function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - main
+ *
+ * Return: void
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ *
+ * @a: number1
+ * @b: number2
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return sum;
+}
