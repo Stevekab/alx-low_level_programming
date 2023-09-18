c311cda5d7e3f93d65cfd36a9d6b8f64e7206c87
Stevekab <stephenmuchiri8584@gmail.com>
2023-09-18 14:08:01 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - take the pointer to an int
+ * the value it points to 98
+ * @n: an integer input pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
